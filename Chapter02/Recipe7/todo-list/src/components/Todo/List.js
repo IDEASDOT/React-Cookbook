868f9d0b30b7cb65582e3dcdef829f085106f708
IDEASDOT <ideasdot@gmail.com>
2020-09-24 18:24:53 -0700

    first commit

--- Chapter02/Recipe7/todo-list/src/components/Todo/List.js
+++ Chapter02/Recipe7/todo-list/src/components/Todo/List.js
@@ -6,11 +6,26 @@ const List = props => (
       <li key={key} className={`${item.completed ? 'completed' : 'pending'}`}>
         {item.task}
 
+
+
+
+           {/**
+             *  If the task is completed we assign the
+             *  .completed class otherwise .pending
+             */}
+
+
         <div className="actions">
           <span className={item.completed ? 'hide' : 'done'} onClick={() => props.markAsCompleted(item.id)}>
             <i className="fa fa-check"></i>
           </span>
 
+
+
+                {/**
+                 * using a callback on the onClick we call
+                 * our removeTask function
+                 */}          
           <span className="trash" onClick={() => props.removeTask(item.id)}>
             <i className="fa fa-trash"></i>
           </span>
